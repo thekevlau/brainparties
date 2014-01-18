0a61f7db02cdc9fd10ff79987ec9836face934b7
Kevin Lau <thekevlau@gmail.com>
2014-01-18 18:42:56 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Brainparties::Application.config.secret_key_base = '229b0b830c1713473578255a0d666ae53197ee27c1b32dd4ae99734fe8c735b6d7bb7c6203396fbb39a4c1b4ae7558d4e9ca84eeed6658fdd4d1dcd2add78e67'
